e6360e0c434ee5bb48a0f66c0ec0c1bb165f53f3
Fedenko Maksym <67603506+feden2906@users.noreply.github.com>
2021-06-23 14:57:30 +0300

    Update Comment.js

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,6 +1,12 @@
-export default function Comment (props){
-    let {item:value} = props
+// export default function Comment (props){
+//     let {item:value} = props
+//     return(
+//         <div>postID {value.postId} - id {value.id} - comment: {value.name}</div>
+//     )
+// }
+                                                               // TODO можна написати так:
+export default function Comment ({item}) {
     return(
-        <div>postID {value.postId} - id {value.id} - comment: {value.name}</div>
+        <div>postID {item.postId} - id {item.id} - comment: {item.name}</div>
     )
 }
